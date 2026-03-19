47a1fdc0973518a4698af60f13be8b9e06bd70ad
purvesh-liv <ppurvesh60@gmail.com>
2026-03-19 15:35:38 +1100

    testing

---
+++ script.js
@@ -0,0 +1,11 @@
+const bill = document.getElementById("bill")
+const tips = document.querySelectorAll(".tip-btn")
+const people = document.getElementById("people")
+const resetBtn = document.getElementById("resetBtn");
+const customTip = document.getElementById("customTip");
+
+
+
+bill.addEventListener("click",()=>{
+    
+})
